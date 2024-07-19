a65ba9c4407505cb423f693601d4c9a25ff6439e
DevBobby <L20011247@orizaba.tecnm>
2024-07-19 17:17:18 -0600

    project base created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'product-list-with-cart-main-angular';
+  isActived = signal(true)
+
+  activated() {
+    this.isActived.set(false)
+  }
+
+  desactivated() {
+    this.isActived.set(true)
+  }
+
 }
